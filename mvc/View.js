b8e3d4eb8c2d28e3ed10d3b398453cbf2c821958
unscriptable <john.hann@e-numera.com>
2010-08-16 15:56:43 -0400

    clarified and refined state() method

--- mvc/View.js
+++ mvc/View.js
@@ -20,35 +20,51 @@ dojo.declare('cujo.mvc.View', [cujo._Widget, cujo._Templated, cujo._Connectable]
     // TODO: use dijit._Widget's get/set? and hook-up watch() in cujo._Widget
     // (detect for existence before adding watch since it's in 1.6)
 
-/*====
-    //  state: String
-    //      Sets the visual state of the View.
-    //      Use set('state', 'edit') and get('state') to set or get the state.
-    //      States can be selected from a mutually-exclusive group by passing a cujo.__StateDef
-    //      object instead of a string.  See cujo.__StateDef in cujo.dom.
-    //      set() returns this widget TODO: return a promise if async
-    //      get() returns all classes set on the object
-    //      Example:
-    //          var states = ['ready', 'done', 'pending'];
-    //          widget.set('state', {state: 'pending', value: true, set: states});
-====*/
-
-    state: function (/* String */ state, /* Boolean|cujo.__StateDef */ value) {
+    state: function (/* String|cujo.__StateDef */ state, /* Boolean? */ value) {
+        //  summary:
+        //      Sets the visual state of the View.
+        //      States can be selected from a mutually-exclusive group by passing a cujo.__StateDef
+        //      object instead of a string.  See cujo.__StateDef in cujo.dom.
+        //      There are three function signatures. See the examples for details.
+        //  description:
+        //      States are converted from camelCase strings to valid HTML-strict class names, e.g.:
+        //      myView.state('bulkDelete') returns true if myView's domNode has the class 'bulk-delete'. 
+        //      TODO: finish documenting all of the possible parameter combos and returns
+        //      Example 1 -- Simple setter:
+        //          myView.state('bulkDelete', false); // turn off 'bulkDelete' mode
+        //      Example 2 -- Sniff for a specific state (getter):
+        //          myView.state('bulkDelete'); // boolean
+        //      Example 3 -- Return all states (getter):
+        //          var array = myView.state();
+        //      Example 4 -- Complex setter:
+        //          var states = ['ready', 'done', 'pending'];
+        //          myView.state({state: 'pending', value: true, set: states});
 
         // disambiguate arguments
-        if (dojo.isObject(state)) {
-            // set
+        if (!dojo.isString(state)) {
+            // set object
+            state.state = cujo.lang.uncamelize(state.state);
             return this._setStateDef(state);
         }
         else if (arguments.length > 1) {
-            // set
-            return this._setStateDef({state: state, value: value});
+            // set string
+            return this._setStateDef({state: cujo.lang.uncamelize(state), value: value});
+        }
+        else if (!!state) {
+            // get array of current states
+            return dojo.map(this._getState().split(' '), function (cn) { return cujo.lang.camelize(cn); });
         }
         else {
-            // get
-            return this._getState(state);
+            // get status of single state (boolean)
+            return this._getState(cujo.lang.uncamelize(state));
         }
 
+    },
+
+    states: function () {
+        //  summary: alias for state() method with no params (getter).
+        //      Returns an array of state strings.
+        return this.state();
     },
 
     block: function () {
@@ -64,7 +80,7 @@ dojo.declare('cujo.mvc.View', [cujo._Widget, cujo._Templated, cujo._Connectable]
     },
 
     _getState: function (/* String */ state) {
-        return cujo.dom.getState(this.domNode, state);
+        return cujo.dom.getState(this.domNode, state) || '';
     },
 
     _setStateDef: function (/* cujo.__StateDef */ stateDef) {
